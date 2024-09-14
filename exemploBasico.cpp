2a0ee5c3ec0800200dfb13d6989249773ca5db60
prof-cringe <professorcringeparaense@gmail.com>
2024-09-13 21:45:51 -0300

    feat:Primeira versao

---
+++ exemploBasico.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    
+    cout << "Exemplo Básico de Aplicativo" << endl;
+    cout << "Digite um numero: ";
+    cin >> a;
+    cout << "Digite outro numero: ";
+    cin >> b;
+    cout << " A soma dos numeros e: " << a + b << endl;
+    system("pause");
+    return 0;
+}
