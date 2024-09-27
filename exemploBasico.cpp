ecf219adb39311209b39408b52de341ce0ca39b1
prof-cringe <professorcringeparaense@gmail.com>
2024-09-27 20:40:31 -0300

    hotfix: Adicionando tratamento para espaço em branco

--- exemploBasico.cpp
+++ exemploBasico.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
     int a;
     int b;
+    string nome;
     
     cout << "Exemplo Básico de Aplicativo" << endl;
+    cout << "Digite o seu nome: ";
+    //cin >> nome;
+    getline(cin, nome);
     cout << "Digite um numero: ";
     cin >> a;
     cout << "Digite outro numero: ";
     cin >> b;
-    cout << " A soma dos numeros e: " << a + b << endl;
+    cout << "Fale " << nome << ", A soma dos numeros e: " << a + b << endl;
     system("pause");
     return 0;
 }
